dca36f7e84243ddc23bae21066fe2bb210f77648
zhaozhao <samoyedsun@gmail.com>
2017-07-30 06:14:59 -0700

    modified server

--- server.cpp
+++ server.cpp
@@ -1,15 +1,8 @@
-#include "my_socket.h"
+#include "engine.h"
 
 int main()
 {
-    MySocket server;
-
-    if (server.create_socket() == false ||
-            server.bind_socket() == false ||
-            server.listen_socket() == false)
-        return 0;
-
-    server.accept_socket();
-	return 0;
+    Engine engine;
+    engine.start();
 }
 
