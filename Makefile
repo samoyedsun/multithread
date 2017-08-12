490dd527732b4b5d4d0d190adb5abc35bf00cf49
Mr_Z <samoyedsun@gmail.com>
2017-08-13 06:19:24 +0800

    update db module

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 all:client_bin server_bin
 
 serverobj=server.o data_packet.o\
-		  engine.o actor.o
+		  engine.o actor.o db.o
 
 clientobj=client.o data_packet.o
 
@@ -9,7 +9,9 @@ client_bin:$(clientobj)
 	g++ -o client_bin $(clientobj)
 
 server_bin:$(serverobj)
-	g++ -o server_bin $(serverobj) -lpthread
+	g++ -o server_bin $(serverobj) \
+		-lpthread -lmysqlclient \
+		-L/usr/lib/mysql
 
 client.o:client.cpp
 	g++ -c client.cpp
@@ -22,9 +24,11 @@ engine.o:engine.cpp engine.h data_packet.h actor.h
 	g++ -c engine.cpp
 actor.o:actor.cpp actor.h data_packet.h
 	g++ -c actor.cpp
+db.o:db.cpp db.h
+	g++ -c db.cpp -I/usr/include/mysql
 
 clean:
 	rm server.o server_bin \
 		client.o client_bin \
 		engine.o actor.o \
-		data_packet.o
+		data_packet.o db.o
---
+++ db.cpp
@@ -0,0 +1,72 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include <mysql/my_global.h>
+#include <mysql/mysql.h>
+
+#include <iostream>
+
+#define MAX_COLUMN_LEN 32
+
+int finish_with_error(MYSQL *con)
+{
+    fprintf(stderr, "%s\n", mysql_error(con));
+    if (con != NULL) mysql_close(con);
+    return -1;
+}
+
+int finish_with_correct(MYSQL *con)
+{
+    std::cout << "mysql client version: " << mysql_get_client_info() << std::endl;
+    std::cout << mysql_get_server_info(con) << std::endl;
+    std::cout << mysql_get_host_info(con) << std::endl;
+    mysql_close(con);
+    return 0;
+}
+
+int create_database()
+{
+    MYSQL *con = mysql_init(NULL);
+    if (NULL == con)
+        return finish_with_error(con);
+
+    if (NULL == mysql_real_connect(con, "localhost", "bjqe", "456789", NULL, 0, NULL, 0))
+        return finish_with_error(con);
+
+    if (mysql_query(con, "CREATE DATABASE IF NOT EXISTS mr_z_db character set utf8"))
+        return finish_with_error(con);
+
+    return finish_with_correct(con);
+}
+
+int test_query()
+{
+    MYSQL *con = mysql_init(NULL);
+    if (NULL == con)
+        return finish_with_error(con);
+
+    if (NULL == mysql_real_connect(con, "localhost", "bjqe", "456789", "mr_z_db", 0, NULL, 0))
+        return finish_with_error(con);
+
+    if (mysql_query(con, "CREATE TABLE IF NOT EXISTS person(name TEXT, password TEXT)"))
+        return finish_with_error(con);
+    if (mysql_query(con, "INSERT INTO person values(\"hunhun\", \"hunhun_pw\")"))
+        return finish_with_error(con);
+
+    return finish_with_correct(con);
+}
+
+int drop_database()
+{
+    MYSQL *con = mysql_init(NULL);
+    if (NULL == con)
+        return finish_with_error(con);
+
+    if (NULL == mysql_real_connect(con, "localhost", "bjqe", "456789", NULL, 0, NULL, 0))
+        return finish_with_error(con);
+
+    if (mysql_query(con, "DROP DATABASE IF EXISTS mr_z_db"))
+        return finish_with_error(con);
+
+    return finish_with_correct(con);
+}
