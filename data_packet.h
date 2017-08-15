6de112ab61f67adab1b48c26874e16039d2f1b49
Mr_Z <samoyedsun@gmail.com>
2017-08-16 07:49:27 +0800

    updata data_packet module

--- data_packet.h
+++ data_packet.h
@@ -15,68 +15,111 @@ class DataPacket
         {
             void *buf_ptr = NULL;
             int new_size = sizeof(val);
-            if (0u == this->block_count_)
+            if (0 == this->block_count_)
             {
-                buf_ptr = malloc(new_size);
-                memcpy(buf_ptr, (void *)&val, new_size);
+                if (NULL == (buf_ptr = malloc(new_size)))
+                {
+                    std::cout << "memory allocation fail!"  << std::endl;
+                    return *this;
+                }
+                if (NULL == memcpy(buf_ptr, (void *)&val, new_size))
+                {
+                    std::cout << "memory copy fail!"  << std::endl;
+                    return *this;
+                }
             }
             else
             {
                 int buf_size = new_size;
                 new_size += this->block_count_;
-                buf_ptr = realloc(this->data_block_, new_size);
-                memcpy((char *)buf_ptr + this->block_count_, (void *)&val, buf_size);
+                if (NULL == (buf_ptr = realloc(this->data_block_, new_size)))
+                {
+                    std::cout << "memory allocation fail!"  << std::endl;
+                    return *this;
+                }
+                if (NULL == memcpy((char *)buf_ptr + this->block_count_, (void *)&val, buf_size));
+                {
+                    std::cout << "memory copy fail!"  << std::endl;
+                    return *this;
+                }
             }
-
-            if (NULL == buf_ptr)
-            {
-                std::cout << "memory allocation fail!"  << std::endl;
-            }
-            else
-            {
-                this->data_block_ = this->pos_ptr_ = buf_ptr;
-                this->block_count_ = new_size;
-            }
-
+            this->data_block_ = this->pos_ptr_ = buf_ptr;
+            this->block_count_ = new_size;
             return *this;
         }
 
-        DataPacket& operator << (std::string &val)
+        DataPacket& operator << (const std::string &val)
         {
-            if (0u >= this->block_count_)
+            if (true == val.empty() || 0 == this->block_count_)
                 return *this;
-           // int16_t str_size;
-
+            void *buf_ptr = NULL;
+            int16_t new_size = val.size();
+            int16_t buf_size = new_size;
+            new_size += sizeof(new_size);
+            new_size += this->block_count_;
+            if (NULL == (buf_ptr = realloc(this->data_block_, new_size)))
+            {
+                std::cout << "memory allocation fail!"  << std::endl;
+                return *this;
+            }
+            int buf_count = this->block_count_;
+            if (NULL == memcpy((char *)buf_ptr + buf_count, (void *)&buf_size, sizeof(buf_size)))
+            {
+                std::cout << "memory copy fail!"  << std::endl;
+                return *this;
+            }
+            buf_count += sizeof(buf_size);
+            if (NULL == memcpy((char *)buf_ptr + buf_count, (void *)val.c_str(), buf_size))
+            {
+                std::cout << "memory copy fail!"  << std::endl;
+                return *this;
+            }
+            this->data_block_ = this->pos_ptr_ = buf_ptr;
+            this->block_count_ = new_size;
+            return *this;
         }
 
         template <typename T>
         DataPacket& operator >> (T &val)
         {
-            if (0u >= this->block_count_)
+            if (0 >= this->block_count_)
                 return *this;
             std::cout << "block_count_:" << this->block_count_ << std::endl;
-
             int ele_size = sizeof(val);
             std::cout << "ele_size:" << ele_size << std::endl;
             char *buf_ptr =  (char *)this->pos_ptr_;
-
-            if (memcpy((void *)&val, buf_ptr, ele_size) != NULL)
+            if (NULL == memcpy((void *)&val, buf_ptr, ele_size))
             {
-
-                this->pos_ptr_ = buf_ptr + ele_size;
-                this->block_count_ -= ele_size;
+                std::cout << "memory copy fail!"  << std::endl;
+                return *this;
             }
-
+            this->pos_ptr_ = buf_ptr + ele_size;
+            this->block_count_ -= ele_size;
             std::cout << "ele_val:" << val << std::endl;
             return *this;
         }
 
         DataPacket& operator >> (std::string &val)
         {
-            if (0u >= this->block_count_)
+            if (0 >= this->block_count_)
                 return *this;
-           // int16_t str_size;
-
+            int16_t buf_size;
+            char *buf_ptr =  (char *)this->pos_ptr_;
+            if (NULL == memcpy((void *)&buf_size, buf_ptr, sizeof(buf_size)))
+            {
+                std::cout << "memory copy fail!"  << std::endl;
+                return *this;
+            }
+            buf_ptr += sizeof(buf_size);
+            char buf_str[buf_size + 1] = {0};
+            if (NULL == memcpy((void *)buf_str, buf_ptr, buf_size))
+            {
+                std::cout << "memory copy fail!"  << std::endl;
+                return *this;
+            }
+            this->pos_ptr_ = buf_ptr + buf_size;
+            this->block_count_ -= buf_size + sizeof(buf_ptr);
+            return *this;
         }
 
         void* get_data_ptr();
